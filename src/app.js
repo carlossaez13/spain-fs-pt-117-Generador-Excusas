3fc04c74cbd642f3b75ee01baa31c04e46b13cdb
carlossaez13 <carlossaez13@gmail.com>
2025-07-22 11:38:15 +0000

    Arreglado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
+    let action = ['ate ', 'peed ', 'crushed ', 'broke '];
+    let what = ['my homework ', 'my phone ', 'the car '];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+    let part1 = Math.floor(Math.random() * who.length);
+    let part2 = Math.floor(Math.random() * action.length);
+    let part3 = Math.floor(Math.random() * what.length);
+    let part4 = Math.floor(Math.random() * when.length);
+
+    document.querySelector("#excuse").innerHTML = who[part1] + action[part2] + what[part3] + when[part4];
+    
+  }
